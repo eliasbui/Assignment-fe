d5e1f0794bbe0eb123fa232f55ebe061e337765f
dthai16 <88380128+thai16-gg@users.noreply.github.com>
2023-02-20 19:14:20 +0700

    done

--- GD-1/js/buy.js
+++ GD-1/js/buy.js
@@ -1,17 +1,57 @@
 var app = angular.module("assignment", []);
 app.controller("assignmentController", function ($scope, $http) {
-  const pageSize = 10;
+  $scope.indexPage = "";
+  $scope.pageSize = "";
+  ($scope.curPage = 1), ($scope.itemsPerPage = 14), ($scope.maxSize = 5);
   $scope.getData = function () {
     $http({
       method: "GET",
       url: "https://63edbc0c388920150dd1f524.mockapi.io/PH22339/wineshop",
     }).then(function getData(response) {
       $scope.wines = response.data;
+      $scope.itemsDetails = response.data;
+      $scope.numOfPages = function () {
+        return Math.ceil($scope.wines.length / $scope.itemsPerPage);
+      };
+      $scope.$watch("curPage + numPerPage", function () {
+        var begin = ($scope.curPage - 1) * $scope.itemsPerPage,
+          end = begin + $scope.itemsPerPage;
+        $scope.filteredItems = $scope.wines.slice(begin, end);
+      });
     });
+  };
+  $scope.totalPage = function () {
+    return $scope.wines.length / $scope.itemsPerPage;
+  };
+  $scope.pageRange = function () {
+    var pageS = $scope.totalPage();
+    $scope.page = pageS;
+    var pageRange = [];
+    for (var i = 0; i < pageS; i++) {
+      pageRange.push(i + 1);
+    }
+    $scope.pageSize = pageRange[pageRange.length - 1];
+    return pageRange;
+  };
+  $scope.changRangePage = function (page) {
+    $scope.curPage = page;
+  };
+  $scope.Previous = function () {
+    $scope.curPage = $scope.curPage - 1;
+    if ($scope.curPage <= 0) {
+      $scope.curPage = 1;
+    }
+  };
+  $scope.Next = function () {
+    $scope.curPage = $scope.curPage + 1;
+    if ($scope.curPage > $scope.totalPage() + 1) {
+      $scope.curPage = $scope.totalPage() + 1;
+    }
   };
   $scope.getData();
   $scope.detailWine = function (wine) {
     $scope.selectWine = wine;
     $scope.showDetail = true;
+    6405;
   };
 });
--- GD-1/js/crud-admin.js
+++ GD-1/js/crud-admin.js
@@ -1,12 +1,49 @@
 var app = angular.module("myApp", []);
 app.controller("AppController", function ($scope, $http) {
+  ($scope.curPage = 1), ($scope.itemsPerPage = 10), ($scope.maxSize = 5);
   $scope.getData = function () {
     $http({
       method: "GET",
       url: "https://63edbc0c388920150dd1f524.mockapi.io/PH22339/wineshop",
     }).then(function getData(response) {
       $scope.wines = response.data;
+      $scope.itemsDetails = response.data;
+      $scope.numOfPages = function () {
+        return Math.ceil($scope.wines.length / $scope.itemsPerPage);
+      };
+      $scope.$watch("curPage + numPerPage", function () {
+        var begin = ($scope.curPage - 1) * $scope.itemsPerPage,
+          end = begin + $scope.itemsPerPage;
+        $scope.filteredItems = $scope.wines.slice(begin, end);
+      });
     });
+  };
+
+  $scope.totalPage = function () {
+    return $scope.wines.length / $scope.itemsPerPage;
+  };
+  $scope.pageRange = function () {
+    var pageS = $scope.totalPage();
+    var pageRange = [];
+    for (var i = 0; i < pageS; i++) {
+      pageRange.push(i + 1);
+    }
+    return pageRange;
+  };
+  $scope.changRangePage = function (page) {
+    $scope.curPage = page;
+  };
+  $scope.Previous = function () {
+    $scope.curPage = $scope.curPage - 1;
+    if ($scope.curPage <= 0) {
+      $scope.curPage = 1;
+    }
+  };
+  $scope.Next = function () {
+    $scope.curPage = $scope.curPage + 1;
+    if ($scope.curPage > $scope.totalPage() + 1) {
+      $scope.curPage = $scope.totalPage() + 1;
+    }
   };
   $scope.getData();
   $scope.detailProduct = function (wine) {
@@ -20,6 +57,7 @@ app.controller("AppController", function ($scope, $http) {
     });
     alert("Successfully deleted,\nPlease refresh your web browser");
   };
+  $scope.idWine = "";
   $scope.nameWine = "";
   $scope.priceWine = "";
   $scope.quantityWine = "";
@@ -37,7 +75,6 @@ app.controller("AppController", function ($scope, $http) {
       price: $scope.priceWine,
       quantity: $scope.quantityWine,
       image: $scope.imageWine,
-      quantity: $scope.quantityWine,
       categories: $scope.categoriesWine,
       country: $scope.countryWine,
       year_of_production: $scope.year_of_productionWine,
@@ -57,9 +94,48 @@ app.controller("AppController", function ($scope, $http) {
         price: $scope.selectWine.price,
         quantity: $scope.selectWine.quantity,
         image: $scope.selectWine.image,
+        categories: $scope.selectWine.categories,
+        country: $scope.selectWine.country,
+        year_of_production: $scope.selectWine.year_of_production,
+        manufacturers: $scope.selectWine.manufacturers,
+        size: $scope.selectWine.size,
+        title: $scope.selectWine.title,
+        reviews: $scope.selectWine.reviews,
       }
     );
-
     alert("success");
+  };
+  $scope.updateProductByID = function (id) {
+    $http.put(
+      "https://63edbc0c388920150dd1f524.mockapi.io/PH22339/wineshop/" + id,
+      {
+        name: $scope.nameWine,
+        price: $scope.priceWine,
+        quantity: $scope.quantityWine,
+        image: $scope.imageWine,
+        categories: $scope.categoriesWine,
+        country: $scope.countryWine,
+        year_of_production: $scope.year_of_productionWine,
+        manufacturers: $scope.manufacturersWine,
+        size: $scope.sizeWine,
+        title: $scope.titleWine,
+        reviews: $scope.reviewsWine,
+      }
+    );
+    alert("success");
+  };
+  $scope.showDetailsClicked = function (wine) {
+    $scope.idWine = wine.id;
+    $scope.nameWine = wine.name;
+    $scope.priceWine = wine.price;
+    $scope.quantityWine = wine.quantity;
+    $scope.imageWine = wine.image;
+    $scope.categoriesWine = wine.categories;
+    $scope.countryWine = wine.country;
+    $scope.year_of_productionWine = wine.year_of_production;
+    $scope.manufacturersWine = wine.manufacturers;
+    $scope.sizeWine = wine.size;
+    $scope.titleWine = wine.title;
+    $scope.reviewsWine = wine.reviews;
   };
 });
